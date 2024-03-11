1145fc13ae15628abcbcd3e89310f234b5cef817
HarshiniAgira <harshiniagira@google.com>
2024-03-11 18:41:10 +0530

    second commit

--- src/test/java/com/mock/RecipeSharingApplication.java
+++ src/test/java/com/mock/RecipeSharingApplication.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MasaiKitchenApplicationTests {
+class RecipeSharingApplication {
 
 	@Test
 	void contextLoads() {
